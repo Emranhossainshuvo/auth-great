f0d1ca84e322568e16b00ac98b2be7aac07a8baf
emran <shuvoorba28@gmail.com>
2023-10-02 16:24:19 +0600

    first commit : initial steps done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAs9lJVMwDGoV6DgMaWEKYKBZDaUULjwYI",
+  authDomain: "auth-great.firebaseapp.com",
+  projectId: "auth-great",
+  storageBucket: "auth-great.appspot.com",
+  messagingSenderId: "340823218766",
+  appId: "1:340823218766:web:de67bfadddc91d684b811f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
